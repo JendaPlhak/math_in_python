a486be4260cad6f2d134bab2a5654a6f514ab7d0
Jenda Plhak <jenda.plhak@gmail.com>
2014-02-22 21:10:11 +0100

    First assignment scripts created

---
+++ intro/spirals.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+from math import floor, sqrt, sin, cos
+
+
+def fist_n_primes(n):           # Calculates first n primes using sieve of Eratosthenes
+
+    sieve = [True] * (n/2)
+    for i in xrange(3, int(n**0.5) + 1, 2):
+        if sieve[i/2]: 
+            sieve[i*i/2::i] = [False] * ((n - i*i - 1)/(2*i) + 1)
+    return [2] + [2*i + 1 for i in xrange(1, n/2) if sieve[i]]
+
+
+def calculate_coordinates_Ulam_spiral(n):    # Calculates [x,y] coordinates of given number n in Ulam spiral using formula 
+                                             # listed on web-page http://danpearcymaths.wordpress.com/2012/09/30/infinity-programming-in-geogebra-and-failing-miserably/
+    p = floor(sqrt(4*n + 1))
+    q = n - floor(p**2 / 4)
+    z = q * 1j**p + ( floor((p + 2)/4) - 1j*floor((p + 1)/4) ) * 1j**(p - 1)
+
+    z *= -1j
+    return [z.real, z.imag]
+
+
+def calculate_coordinates_archimedian_spiral(n):
+
+    angle = n * 0.05
+    x     = (1 + angle)*cos(angle)
+    y     = (1 + angle)*sin(angle)
+    return [x, y]
+
+
+def plot_and_save_spiral(n, spiral_fun, path):
+
+    primes        = fist_n_primes(n)
+    coord_primes  = zip(*[spiral_fun(i) for i in primes])
+    
+    fig = plt.figure(figsize=(23.5, 23.5)) 
+    plt.plot(coord_primes[0], coord_primes[1],    'ro')
+
+    if spiral_fun == calculate_coordinates_Ulam_spiral:
+
+        coord_quad_form = {}   # Interesting patterns can be observed when highlighting primes of form 4n + an + b
+        for i in xrange(n):
+            coord_quad_form[4*i**2 + 7*i - 49] = None 
+            if 4*i**2+3*i - 19 > primes[-1]:
+                break
+        coord_quad_form = zip(*[spiral_fun(i) for i in primes if i in coord_quad_form])
+        plt.plot(coord_quad_form[0], coord_quad_form[1], 'bo')
+    else:
+        coord_spiral  = zip(*[spiral_fun(i) for i in range(n)])
+        plt.plot(coord_spiral[0], coord_spiral[1], 'k')
+
+    plt.axis([ min(coord_primes[0]) - 1, max(coord_primes[0]) + 1, min(coord_primes[1]) - 1, max(coord_primes[1]) + 1 ])
+
+    fig.savefig(path, dpi=80, bbox_inches='tight')
+    plt.clf()
+
+
+n = 100000
+plot_and_save_spiral(n, calculate_coordinates_Ulam_spiral,        "Ulam_spiral.png");
+plot_and_save_spiral(n, calculate_coordinates_archimedian_spiral, "archimed_spiral.png");
