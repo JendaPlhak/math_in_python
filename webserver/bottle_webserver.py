0a72359cdf4bfe75976db9ab6d149d7e1492abdd
Jenda Plhak <jenda.plhak@gmail.com>
2014-03-01 15:28:00 +0100

    path changed

--- webserver/bottle_webserver.py
+++ webserver/bottle_webserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append("../2_cviceni")
+sys.path.append("2_cviceni/")
 
 from bottle import route, run, template, static_file
 from pascals_triangle import plot_pascals_triangle
@@ -13,6 +13,7 @@ def index():
 
 @route('/pascal/<param_string>')
 def index(param_string):
+    print "wuiiiii"
     params = param_string.split("&")
     return plot_pascals_triangle(int(params[0]),int(params[1]))
 
@@ -20,4 +21,3 @@ def index(param_string):
 def index(name):
     return '<b>ahoj {0}</b>\n<img src="http://25.media.tumblr.com/93a200d60363ff4e6b928d1ad1174c22/tumblr_n126y7hHEQ1tt6sj1o1_400.gif" alt="wiiiii">'.format(name)
 run(host='localhost', port=8080)
-
