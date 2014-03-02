d943b1fae3594d8883fa866b677c6b528241a2e4
quapka <quapka@gmail.com>
2014-03-02 22:58:25 +0100

    prvni layout

---
+++ webserver/layout2/routes.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/plhak')
+def plhak():
+	return render_template('plhak.html')
+
+@app.route('/kvapil')
+def kvapil():
+	return render_template('kvapil.html')
+if __name__ == '__main__':
+	app.run()
