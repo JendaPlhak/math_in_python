ebc71a40b667ec7a4f47d892a05144b6fa4a6c26
quapka <quapka@gmail.com>
2014-03-12 17:32:40 +0100

    commitiim

--- webserver/layout2/routes.py
+++ webserver/layout2/routes.py
@@ -9,8 +9,19 @@ def home():
 def plhak():
 	return render_template('plhak.html')
 
+
+@app.route('/plhak/week/task')
+def task():
+	return render_template('1task.html')
+
+"""
+@app.route('/plhak/week=<numWeek>/task=<numTask>')
+def task():
+	return render_template('plhak/<numWeek>week/<numTask>task.html')
+"""
 @app.route('/kvapil')
 def kvapil():
 	return render_template('kvapil.html')
+
 if __name__ == '__main__':
 	app.run()
