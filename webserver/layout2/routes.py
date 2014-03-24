1ff5496a63336163979c4297d512f90976a1e92a
Jenda Plhak <jenda.plhak@gmail.com>
2014-03-24 17:01:09 +0100

    import of logging fixed

--- webserver/layout2/routes.py
+++ webserver/layout2/routes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-
+import logging
 import sys
 sys.path.append("../../JendasWork/2_task/")
 sys.path.append("/home/ubuntu/math_in_python/JendasWork/2_task/")
 
-from flask import Flask, render_template
+from logging.handlers import RotatingFileHandler
+from flask            import Flask, render_template
+
 app = Flask(__name__)
 
 from urlparse         import parse_qs
