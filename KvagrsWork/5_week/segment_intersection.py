0383a5fb8c326ce5123b08ae0ee5ac4e40cc3de7
quapka <quapka@gmail.com>
2014-03-25 22:42:04 +0100

    correct path from home to svg image

---
+++ KvagrsWork/5_week/segment_intersection.py
@@ -0,0 +1,93 @@
+import svgwrite
+from random import random, randint
+from math 	import sqrt, cos, sin
+
+def dist(A,B):
+
+	return sqrt( (A[0]-B[0])**2 + (A[1]-B[1])**2 )			# euclidean distance/norm
+
+
+def cross_point(u, v):										# u, v are lists, |u| = 4 = |v|
+
+	# 	Using intersection of two lines
+	#
+	# 	s: y = k1*x + q1
+	# 	t: y = k1*x + q1
+
+	A,B,C,D = u[:2],u[2:],v[:2],v[2:]
+	x0,y0 = A
+	x1,y1 = B
+	x2,y2 = C
+	x3,y3 = D
+	
+	if x0 - x1 == 0 or x2 - x3 == 0:
+		return (None, None), False
+
+	k1 = (y0-y1)/(x0-x1)									# actual magic, k1,2 and q1,2 are coefficients for lines, 
+	q1 = y0-k1*x0
+	k2 = (y2-y3)/(x2-x3)
+	q2 = y2-k2*x2
+
+	if k1 == k2:
+		return (None, None), False
+
+	Px = (q2-q1)/(k1-k2)									# [Px,Py] is the cross point
+	Py = k1*Px+q1
+
+	P = [Px,Py]
+
+	if 	abs(dist(A,B) - dist(A,P) - dist(P,B)) < 0.01 and \
+		abs(dist(C,D) - dist(C,P) - dist(P,D)) < 0.01 and \
+		A not in [C,D] and B not in [C,D]:
+		return (Px,Py), True
+
+	else:
+		return (None, None), False
+
+
+def generate_data(n, l=100, segments=False):
+
+	data = []
+	for i in xrange(n):
+		x = random()*2*l+l  								# generate random x,y starting coordinates
+		y = random()*2*l+l
+		if segments:
+			angle = randint(0,360) 							# generate random angle 
+			nx = x+l*cos(angle) 							# calculate new coordinates
+			ny = y+l*sin(angle)
+			data.append([x,y,nx,ny])
+		else:
+			data.append([x,y])
+	return data	
+
+
+def segment_intersection(n, length=100):
+
+	segments = generate_data(n, length, segments=True)
+	tmp_list = list(segments)
+	seg_intersect = []
+	N = 0
+	for segA in segments:									# iterate over all segments
+		tmp_list.remove(segA)								# remove the one you have started from
+
+		for segB in tmp_list:
+			if cross_point(segA, segB)[1]:
+				seg_intersect.append(cross_point(segA, segB)[0])
+	return	segments, seg_intersect
+
+
+if __name__ == '__main__':
+
+	im = svgwrite.drawing.Drawing()
+	data = segment_intersection(30, length=120)
+
+	for line in data[0]:
+		im.add( im.line(start 	= (line[0],line[1]),\
+						end 	= (line[2],line[3]),\
+						stroke  = 'black'))
+	for point in data[1]:
+		im.add( im.circle(	center 	= point,\
+							r 		= 2,\
+							stroke	= 'red'))
+
+	im.saveas("segment_intersection.svg")
---
+++ KvagrsWork/5_week/triangulation.py
@@ -0,0 +1,74 @@
+import svgwrite
+from random 				import random, randint
+from math 					import sqrt, cos, sin
+from segment_intersection 	import cross_point, generate_data, dist
+
+
+def join_points(points):								# creates a segment for each pair of points
+
+	segments = []
+	tmp_list = points[:]
+
+	for A in points:
+		tmp_list.remove(A)
+		for B in tmp_list:
+				segments.append(A+B)
+	return segments
+
+
+def sort_segments(segments):
+
+	for i in xrange(len(segments)):
+		for j in xrange(i,len(segments)):
+			if dist(segments[i][2:],segments[i][:2]) >= dist(segments[j][2:],segments[j][:2]):
+				tmp  = segments[j]
+				segments[j] = segments[i]
+				segments[i] = tmp
+	return
+
+
+def triangulation(n, length=100, min_side=False):
+
+	points 		= generate_data(n, length, segments=False)
+	segments 	= join_points(points)
+
+	if min_side:							# If True sorts segments by the length of the side of triangles
+		sort_segments(segments)
+
+	fin_segments= [segments[0]]
+	crosses 	= []
+
+	segments.pop(0)
+	for seg1 in segments:
+		add = True
+		for seg2 in fin_segments:
+			if cross_point(seg1, seg2)[1] == True:
+				add = False
+				break
+		if add:
+			fin_segments.append(seg1)
+	
+	return points, fin_segments
+
+def draw_triangulation(n):
+
+	im = svgwrite.drawing.Drawing()
+	points, segments = triangulation(50, length=120, min_side=True)
+
+	for seg in segments:
+		im.add( im.line(	start 	= (seg[0], seg[1]),\
+							end 	= (seg[2], seg[3]),\
+							stroke 	= 'black'))
+	for point in points:
+		im.add( im.circle( 	center	= (point[0],point[1]),\
+							r 		= 2,\
+							stroke 	= 'black'))
+
+	im.saveas("/home/ubuntu/math_in_python/webserver/layout2/static/img/KvagrsWork/5_week/triangulation.svg")#static/img/KvagrsWork/5_week/
+
+	return "/home/ubuntu/math_in_python/webserver/layout2/static/img/KvagrsWork/5_week/triangulation.svg"
+
+
+if __name__ == '__main__':
+
+	draw_triangulation(10)
