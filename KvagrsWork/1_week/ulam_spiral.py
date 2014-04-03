2a88cea4153124d76815195adeae00f522ca416e
quapka <quapka@gmail.com>
2014-04-04 00:33:05 +0200

    pics, scripts, html

---
+++ KvagrsWork/1_week/ulam_spiral.py
@@ -0,0 +1,18 @@
+def sieveOfEratosthenes(n):
+
+	numbers = [True] * n
+	i = 2
+	while i**2 < n:
+		for j in range(2,int(n/i)+1):
+			numbers[(i*j)-1] = False
+
+		i += 1
+
+	primes = []
+	for j in range(1,n):
+		if numbers[j] == True:
+			primes.append(j+1)
+
+	return primes
+
+sieveOfEratosthenes(1000)
