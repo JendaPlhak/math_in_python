f89088dc696edcbe1761c905ee113b038707b499
Jan Plhak <jenda.plhak@gmail.com>
2014-05-13 14:57:09 +0200

    Autodeployment fixed

--- git_autodeploy/createArticles.sh
+++ git_autodeploy/createArticles.sh
@@ -2,7 +2,7 @@
 
 for STUD in "Jendas" "Kvagrs";
 do
-    for TASK in `ls ../${STUD}Work/ | grep ".*task"`
+    for TASK in `ls ${1}/../${STUD}Work/ | grep ".*task"`
     do
         python article.py ../${STUD}Work/${TASK}/cmt
     done
