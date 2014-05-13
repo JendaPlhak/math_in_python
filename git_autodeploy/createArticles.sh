01a189e0f75c45af31c7d5b1cf03eca0f14a5735
Jan Plhak <jenda.plhak@gmail.com>
2014-05-13 15:06:06 +0200

    path finding for articles

--- git_autodeploy/createArticles.sh
+++ git_autodeploy/createArticles.sh
@@ -4,6 +4,6 @@ for STUD in "Jendas" "Kvagrs";
 do
     for TASK in `ls ${DIR}/../${STUD}Work/ | grep ".*task"`
     do
-        python article.py ${DIR}/../${STUD}Work/${TASK}/cmt
+        python ${DIR}/article.py ${DIR}/../${STUD}Work/${TASK}/cmt
     done
 done
