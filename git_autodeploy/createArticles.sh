a7ab81d703becb05aa3b298c20172bcd3dbbca48
Jan Plhak <jenda.plhak@gmail.com>
2014-05-17 18:00:50 +0200

    create articles nicer print

--- git_autodeploy/createArticles.sh
+++ git_autodeploy/createArticles.sh
@@ -3,6 +3,7 @@ for STUD in "Jendas" "Kvagrs";
 do
     for TASK in `ls ../${STUD}Work/ | grep ".*task"`
     do
+        echo Processing ${STUD}Work/${TASK}
         python article.py ../${STUD}Work/${TASK}/cmt
     done
 done
