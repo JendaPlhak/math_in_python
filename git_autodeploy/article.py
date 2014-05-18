5f9966aaff0d586562f7eb4ab97e9157a19f53b0
quapka <quapka@gmail.com>
2014-05-18 22:01:29 +0200

    valid regexp

--- git_autodeploy/article.py
+++ git_autodeploy/article.py
@@ -116,7 +116,7 @@ for _file in os.listdir( directory ):
                 #users      = { "JendasWork" : "plhak", "KvagrsWork" : "kvapil"}
                 user, task = part_dir.split('/')[:2]
                 #name       = users[ user ]
-                num_task   = re.match(r'(\d)', task).group(0)
+                num_task   = re.match(r'(\d+)', task).group(0)
 
                 article.write('<div class="menuCode">'\
                                +'   <ul>'\
