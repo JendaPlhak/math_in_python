484e93225d09aaf4fc74b050157fa56f6e33ef5f
quapka <quapka@gmail.com>
2014-06-07 16:27:32 +0200

    effects mostly done

--- KvagrsWork/4_task/basic_shapes.py
+++ KvagrsWork/4_task/basic_shapes.py
@@ -1,145 +1,169 @@
 #! usr/bin/env python
 
-from PIL 	   import Image
-from math 	   import tan, sqrt, pi, cos, sin, floor, ceil
+from PIL       import Image
+from math      import tan, sqrt, pi, cos, sin, floor, ceil
 from itertools import product
 
-""" Example of Image.new and putpixel - puts black pixel at [100,100] """
-#im = Image.new("RGB", (2300, 200), "white")
-#im.putpixel((100,100), (0,0,0))
-#im.show()
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
-def circle(r, x0=100, y0=100, fill=False, eps = 0.05):
+def circle(rad, x0=100, y0=100, fill=False, eps = 0.05):
 # r = radius, the center of the circle = [x0,y0]
 
-	coordinates = []
-	for x in xrange(-r,r):
-		for y in xrange(-r,r):
-			if fill and x**2 + y**2 - r**2 <= eps :				# fill == True, therefore fill it!
-				coordinates.append([x+x0,y+y0])				# creating the circle somwhere, and then moving it 
-				print "fill"
-			if ~fill and abs((x**2 + y**2 - r**2)/float(r**2)) <= eps:	# fill == False 
-				coordinates.append([x+x0,y+y0])
+    coordinates = []
+    for x in xrange(-rad,radd):
+        for y in xrange(-rad,rad):
+            if fill and x**2 + y**2 - r**2 <= eps :
+                coordinates.append([x + x0,y + y0])
+                print "fill"
+            if ~fill and abs((x**2 + y**2 - r**2) / float(r**2)) <= eps:
+                coordinates.append([x + x0,y + y0])
 
-	return coordinates
-
-
-def circle_par(r, start, end, thickness=20, fill=True):		# r = radius, start/end, absolute angles
-
-	coordinates = []
-	for t in xrange(start, end):
-		r_t = (t*pi)/180									# Convert degrees to radians
-		x = r+int(r*cos(r_t))								# Calculate coordinates		
-		y = r+int(r*sin(r_t))
-		coordinates.extend(circle(thickness, x+r, y+r, fill))
-
-	return coordinates
+    return coordinates
 
 
-def ellipse_par(a, b, start=0, end=360, thickness=5, fill=True):	# a, b axis, start/end, absolute angles
+def circle_par(r, start, end, thickness=20, fill=True):     # r = radius, start/end, absolute angles
 
-	coordinates = []
-	for t in xrange(start, end):
-		r_t = (t*pi)/180									# Convert degrees to radians
-		x = a+int(a*cos(r_t))								# Calculate coordinates		
-		y = b+int(b*sin(r_t))
-		coordinates.extend(circle(thickness, x+a, y+b, fill))
+    coordinates = []
+    for t in xrange(start, end):
+        r_t = (t * pi) / 180
+        x = r + int(r * cos(r_t))
+        y = r + int(r * sin(r_t))
+        coordinates.extend(circle(thickness, x + r, y + r, fill))
 
-	return coordinates
+    return coordinates
+
+
+def ellipse_par(a, b, start=0, end=360, thickness=5, fill=True):
+# a, b axis, start/end, absolute angles
+
+    coordinates = []
+    for t in xrange(start, end):
+        # Convert degrees to radians
+        r_t = (t*pi)/180
+        # Calculate coordinates
+        x = a + int(a * cos(r_t))
+        y = b + int(b * sin(r_t))
+        coordinates.extend(circle(thickness, x + a, y + b, fill))
+
+    return coordinates
 
 
 def ellipse(a, b, eps=0.05):
 
-	coords = []
-	for x, y in product(range(-a, a), range(-b, b)):
-		if (x / float(a))**2 + (y / float(b))**2 - 1 <= eps:
-			coords.extend([ [x, y] ])
-			
-	return coords
+    pixs = []
+    cols = []
+    for x, y in product(range(-a*2, a*2), range(- 2*b, 2*b)):
+        n_a = a * 1.
+        n_b = b * 1.
+
+        if (x / n_a)**2 + (y / n_b)**2 + (x * y) / (n_a * n_b) - 1 <= eps:
+            pixs.append( [x, y] )
+            col = [sin( (x / (n_a) )**2 + (y / n_b)**2 + (x * y) / (n_a * n_b))]
+            col = tuple( map ( lambda x: int(127 * x), 3 * col))
+            cols.append( col )
+
+    pixs = pixs_and_cors(pixs, cols)
+
+    return pixs
 
 
-def spiral_par(start=0, end=3*360, thickness=3, fill=True):	# start/end, absolute angles
+def spiral_par(start=0, end=3*360, thickness=3, fill=True):
+# start/end, absolute angles
 
-	# Calculate the offset
-	#r_t = ((end%360)*pi)/180
-	#a = int(end*cos(r_t) )
-	#b = int(end*sin(r_t))
-	coordinates = []
-	for t in xrange(start, end):
-		r_t = (t*pi)/180									# Convert degrees to radians
-		x = int(cos(r_t))									# Calculate coordinates		
-		y = int(sin(r_t))
-		
-		coordinates.extend(circle(thickness, x, y, fill))
+    # Calculate the offset
+    #r_t = ((end%360)*pi)/180
+    #a = int(end*cos(r_t) )
+    #b = int(end*sin(r_t))
+    coordinates = []
+    for t in xrange(start, end):
+        r_t = (t*pi)/180
+        x = int(cos(r_t))
+        y = int(sin(r_t))
+        
+        coordinates.extend(circle(thickness, x, y, fill))
 
-	#print coordinates
-	return coordinates
+    #print coordinates
+    return coordinates
 
 
 def equilateral_triangle(side):
 
-	coordinates = []
-	#print -side/2, side/2,int((sqrt(3)*side)/2)
-	for x in xrange(-side/2, side/2):
-		for y in xrange(int((sqrt(3)*side)/2)):
-			v = (sqrt(3)*side)/2
-			if y <= sqrt(3)*x+v and y <= -sqrt(3)*x+v:
-				coordinates.append([x+side,-y+side])		# minus y-coordinate - [0,0] is top left and pos. directions are v and >
+    coordinates = []
+    #print -side/2, side/2,int((sqrt(3)*side)/2)
+    for x in xrange(-side / 2, side / 2):
+        for y in xrange(int( (sqrt(3) * side) / 2)):
+            alt = (sqrt(3) * side) / 2
+            if y <= sqrt(3) * x + alt and y <= -sqrt(3) * x + alt:
+                # minus y-coordinate - [0,0] is top left and pos. directions are v and >
+                coordinates.append([x + side,-y + side])
 
-	return coordinates
+    return coordinates
+
+
+def pixs_and_cors(pixs, cols):
+# shifts pixels and then adds corresponding colors
+
+    pixs, size = movePointsTowardsOrigin( pixs )
+    pixs       = [ size ] + [ [pixs[i], cols[i]] for i in xrange(len(pixs))]
+
+    return pixs
 
 
 def movePointsTowardsOrigin(points):
 
-	minX = points[0][0]
-	minY = points[0][1]
-	maxX = points[0][0]
-	maxY = points[0][1]
+    minX = points[0][0]
+    minY = points[0][1]
+    maxX = points[0][0]
+    maxY = points[0][1]
 
-	for point in points:
-		if minX > point[0]: minX = point[0]
-		if minY > point[1]:	minY = point[1]
-		if maxX < point[0]:	maxX = point[0]
-		if maxY < point[1]:	maxY = point[1]
+    for point in points:
+        if minX > point[0]: minX = point[0]
+        if minY > point[1]: minY = point[1]
+        if maxX < point[0]: maxX = point[0]
+        if maxY < point[1]: maxY = point[1]
 
-	minA = min(minX, minY)
+    minA = min(minX, minY)
 
-	for point in points:
-		point[0] -= minX
-		point[1] -= minY
+    for point in points:
+        point[0] -= minX
+        point[1] -= minY
 
-	size = max(maxX - minX, maxY - minY) + 1
-	return points, size	
+    # determine the size of the canvas
+    size = [ maxX - minX + 1, maxY - minY + 1]
+
+    return points, size 
 
 
-def draw(coordinates, filename=''):
-	
-	coordinate, size = movePointsTowardsOrigin(coordinates)
-	im = Image.new("RGB", (size,size), "white")
-	for pixel in coordinates:
-		r = pixel[0]
-		g = pixel[1]
-		b = pixel[0] + pixel[1]+100
-		#b = (200)
+def draw(pixs, filename=''):
+    
+    size = pixs[0]
+    pixs = pixs[1::]
 
-		color = (r,g,b)
-		im.putpixel( pixel, color)
+    im = Image.new("RGB", (size[0],size[1]), "white")
+    for pix in pixs:
+    #    r = pix[0]
+    #    g = pix[1]
+    #    b = pix[0] + pix[1]+100
+    #    #b = (200)
+#
+#    #    color = (r,g,b)
+        im.putpixel( pix[0], pix[1])
 
-	if filename:
-		im.save('img/'+ filename + ".bmp")
-	else:
-		im.show()
+    if filename:
+        im.save('img/'+ filename + '.png')
+    else:
+        im.show()
 
 
 if __name__ == '__main__':
 
-	#draw(circle(100, 300,300, fill=True), filename="circle_fill")
-	#draw(circle(100, 300,300, fill=False), filename="circle")
-	#draw(circle_par(100, 0, 360, thickness=5, fill=True), filename="circle_par")
-	#draw(equilateral_triangle(100), filename="equilateral_triangleA")
-	#draw(ellipse_par(100,50), filename="spiral_par")
-	#draw(spiral_par())	
-	draw(ellipse(100, 50), 'ellipse')
+    #draw(circle(100, 300,300, fill=True), filename="circle_fill")
+    #draw(circle(100, 300,300, fill=False), filename="circle")
+    #draw(circle_par(100, 0, 360, thickness=5, fill=True), filename="circle_par")
+    #draw(equilateral_triangle(100), filename="equilateral_triangleA")
+    #draw(ellipse_par(100,50), filename="spiral_par")
+    #draw(spiral_par()) 
+        draw(ellipse(100, 50), 'ellipse')
+    #ellipse(10, 5)
--- KvagrsWork/4_task/effects.py
+++ KvagrsWork/4_task/effects.py
@@ -1,79 +1,90 @@
 #! usr/bin/env python
 
-from PIL  	   import Image
-from math 	   import cos, sin, tan, pi
+from PIL       import Image
+from math      import cos, sin, tan, pi
 from itertools import product
+
+radius = lambda (x,y): (x**2 + y**2)**0.5
 
 def chessboard(frame=500, side=25, filename=''):
 
-	im = Image.new("RGB", (frame, frame), (255,255,255))
-	n = frame / 2
-	for x, y in product(xrange(-n, n), xrange(-n, n)):
-		mod_x = x % (side * 2)
-		mod_y = y % (side * 2)
+    im = Image.new("RGB", (frame, frame), (255,255,255))
+    n = frame / 2
+    for x, y in product(xrange(-n, n), xrange(-n, n)):
+        mod_x = x % (side * 2)
+        mod_y = y % (side * 2)
 
-		if x**2 + y**2 - 10**4 < 0:
-			if (mod_x < side) != (mod_y < side):
-				col = (0,0,0)
-			else:
-				col = (255,255,255)
-		else:
-			if (mod_x < side) != (mod_y < side):
-				col = (255,255,255)
-			else:
-				col = (0,0,0)
+        if radius((x, y)) % 210 < 105:
+        #if x**2 + y**2 - 10**4 < 0:
+            if (mod_x < side) != (mod_y < side):
+                col = (0,0,0)
+            else:
+                col = (255,255,255)
+        else:
+            if (mod_x < side) != (mod_y < side):
+                col = (255,255,255)
+            else:
+                col = (0,0,0)
 
 
-		im.putpixel((x + n, y + n), col)
+        im.putpixel((x + n, y + n), col)
 
-	if filename:
-		im.save('img/'+ filename +'.png')
-	else:
-		im.show()
+    if filename:
+        im.save('img/'+ filename +'.png')
+    else:
+        im.show()
 
-	return
-	
-	
+    return
+    
+    
 
-def circles(side=500, filename=''):
-	
-	im = Image.new("RGB", (side, side), (255,255,255))
-	n = side / 2
-	for x, y in product(xrange(-n, n), xrange(-n, n)):	
-		#RBG_tuple = tuple( map( lambda x: int(x), [255 * sin(x**2), 255 * cos(y**2), 0 ] ) ) # irish
-		#RBG_tuple = tuple( map( lambda x: int(x), [255 * sin(pi * x * 0.09), 255 * cos(pi * y * 0.09), 255 * sin(pi * x * y * 0.09)] ) ) # color circle
-		#RBG_tuple = tuple( map( lambda x: int(x), [255 * sin(pi * x * 0.09), 255 * cos(pi * y * 0.09), 100 *(pi * y * 0.09)] ) ) #water
-		#RBG_tuple = tuple( map( lambda x: int(x), 3 * [255 * (cos(x / 5)**2 + sin(y / 5)**2)**0.5]) ) # modern squares
-		#RBG_tuple = tuple( map( lambda x: int(x), [x+y,x-y,x*y]) )
-		
-		if max(abs(x), abs(y)) < 100:
-			RBG_tuple = tuple( map( lambda x: int(x), 3 * [ 127 * (sin((x**2 +  y**2)**0.5 / pi / 2) + 1)]) )
-		else:
-			RBG_tuple = tuple( map( lambda x: int(x), 3 * [ 255 - 127 * (sin((x**2 +  y**2)**0.5 / pi / 2) + 1)]) )
+def circles(side=500, filename='', color=None):
+    
+    im = Image.new("RGB", (side, side), (255,255,255))
+    n = side / 2
+    for x, y in product(xrange(-n, n), xrange(-n, n)):  
+        
+        if color:
+            RBG_tuple = tuple( map( lambda z: int(z), color((x, y)) ) )
+        else:
+            if max(abs(x), abs(y)) < 100:
+                RBG_tuple = tuple( map( lambda x: int(x), 3 * [ 127 * (sin((x**2 +  y**2)**0.5 / pi / 2) + 1)]) )
+            else:
+                RBG_tuple = tuple( map( lambda x: int(x), 3 * [ 255 - 127 * (sin((x**2 +  y**2)**0.5 / pi / 2) + 1)]) )
 
-		im.putpixel( (x + n, y + n), RBG_tuple )
+        im.putpixel( (x + n, y + n), RBG_tuple )
 
-	if filename:
-		im.save('img/'+ filename +'.png')
-	else:
-		im.show()
+    if filename:
+        im.save('img/'+ filename +'.png')
+    else:
+        im.show()
 
 
 def lines(side=500, filename=''):
 
-	im = Image.new("RGB", (side, side), (255,255,255))
-	n = side / 2
-	for x, y in product(xrange(-n, n), xrange(-n, n)):	
-		RBG_tuple = tuple( map( lambda x: 127 * int(x), [max(x, 0), max(y,0), max(x, y)]) )
-		im.putpixel( (x + n, y + n), RBG_tuple )
+    im = Image.new("RGB", (side, side), (255,255,255))
+    n = side / 2
+    for x, y in product(xrange(-n, n), xrange(-n, n)):  
+        RBG_tuple = tuple( map( lambda x: 127 * int(x), [max(x, 0), max(y,0), max(x, y)]) )
+        im.putpixel( (x + n, y + n), RBG_tuple )
 
-	if filename:
-		im.save('img/'+ filename +'.png')
-	else:
-		im.show()
+    if filename:
+        im.save('img/'+ filename +'.png')
+    else:
+        im.show()
 
 if __name__ == '__main__':
 
-	#circles(filename='circle')
-	#chessboard(filename='chessboard')
-	lines(filename='lines')
+    types = [lambda (x, y): [255 * sin(x**2), 255 * cos(y**2), 0 ],
+             lambda (x, y): [255 * sin(pi * x * 0.09), 255 * cos(pi * y * 0.09), 255 * sin(pi * x * y * 0.09)],
+             lambda (x, y): [255 * sin(x / 10.), 255 * sin((x + y) / 10.), 255 * sin(y / 10.)],
+             lambda (x, y): [255 * sin(x / 10), 255 * sin((x + y) / 10), 255 * sin(y / 10)],
+             lambda (x, y): 3 * [255 * (cos(x / 5)**2 + sin(y / 5)**2)**0.5],
+             lambda (x, y): [x + y, x - y, x * y]]
+
+    for i, color in enumerate(types):
+        circles(filename='circle_'+ str(i + 1), color=color)
+
+    circles(filename='circle')
+    chessboard(filename='chessboard')
+    lines(filename='lines')
