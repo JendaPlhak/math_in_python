bf2f50245332bd841a8303a8b2c8bafb619c61eb
quapka <quapka@gmail.com>
2014-06-22 20:30:23 +0200

    num maze magic

--- webserver/layout2/static/script.js
+++ webserver/layout2/static/script.js
@@ -61,7 +61,7 @@ $(document).ready(function(){
         "7" : ["complex_fractals"],
         "8" : ["affine_transformation","mrcm"],
         "9" : ["linear_regression","data_cluster"],
-        "10": ["number_maze","mazelib"],
+        "10": ["num_maze","mazelib"],
         "11": ["empty"]
     }
 
